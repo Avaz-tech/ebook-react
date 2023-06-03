7c31d9b062d7e17aca25a6eac37acb8fcad6e34c
Ben96 <avazravshan0@gmail.com>
2023-06-03 15:13:21 +0900

    change navbar color when clicked on using context api

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,10 +3,20 @@ import { Navbar, Nav } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../styles/NavigationBar.css'
 
+import { useTheme } from '../hooks/useTheme';
+
+
+
+
 
 function NavigationBar() {
+  
+ const { color, changeColor } = useTheme();
+
+
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar  style={{ background: color }} expand="lg"  onClick={() => changeColor('pink') }>
       <Navbar.Brand href="#">Hittbook</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
